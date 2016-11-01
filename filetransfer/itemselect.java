760777315d08b5dd8a15ea67a399d26be2b8ccae
fordesta <fordesta@users.noreply.github.com>
2016-11-01 15:51:56 -0700

    Update itemselect.java

--- filetransfer/itemselect.java
+++ filetransfer/itemselect.java
@@ -1,6 +1,7 @@
 //This method selects a list of items pre-defined in a text file and then compares them to the input
 //If an item from the list matches the input then the input is returned as a valid category
 //Otherwise the system notes that the item does not exist
+import java.io.*;
 
 public String itemSelect(search String){
     String res = "Item not found";
