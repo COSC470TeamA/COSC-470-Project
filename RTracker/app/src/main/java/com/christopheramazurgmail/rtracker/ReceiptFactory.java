f28272495da4e5b2c9a525b44e32233192b2c723
jebm <brewiebe@gmail.com>
2016-11-06 21:53:37 -0800

    AT-75 Include layout content programatically

---
+++ RTracker/app/src/main/java/com/christopheramazurgmail/rtracker/ReceiptFactory.java
@@ -0,0 +1,13 @@
+package com.christopheramazurgmail.rtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiptFactory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt_factory);
+    }
+}
