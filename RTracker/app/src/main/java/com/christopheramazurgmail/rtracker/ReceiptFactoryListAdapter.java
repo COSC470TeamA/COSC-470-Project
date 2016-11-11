09a6141c41508d8d44fb827ece69938ff286e065
jebm <brewiebe@gmail.com>
2016-11-11 13:00:56 -0800

    AT-75: Created Receipt Factory

---
+++ RTracker/app/src/main/java/com/christopheramazurgmail/rtracker/ReceiptFactoryListAdapter.java
@@ -0,0 +1,10 @@
+package com.christopheramazurgmail.rtracker;
+
+import android.widget.ListAdapter;
+
+/**
+ * Created by brewi on 2016-11-10.
+ */
+
+public class ReceiptFactoryListAdapter implements ListAdapter {
+}
