dbe2575ab515b956d7a62c17890fc333782e7fb8
christopheramazur <christopheramazur@gmail.com>
2016-11-24 15:37:53 -0800

    CM_103_107_Crop_Precision_Rectangles
    Moved remote library dependency into working folder because I needed to look at it more often than I'd have liked.

--- RTracker/app/src/main/java/com/christopheramazurgmail/rtracker/takephoto/TakePhotoActivity.java
+++ RTracker/app/src/main/java/com/christopheramazurgmail/rtracker/takephoto/TakePhotoActivity.java
@@ -6,25 +6,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.android.camera.CropImageIntentBuilder;
 import com.christopheramazurgmail.rtracker.R;
-import com.christopheramazurgmail.rtracker.tesseract.OCRActivity;
 
 
 public class TakePhotoActivity extends Activity {
 
-    Button OCRActivity;
     Button takePicture;
-    ImageButton cropPicture;
     ImageView imagePreview;
     int CAMERA_PIC_REQUEST = 1;
-    int CAMERA_CROP_REQUEST = 2;
-    //int CAMERA_QUICK_PIC_REQUEST = 3; - for taking a quick picture with no confirmation stuff
-    Uri imageUri;
+    int REQUEST_CROP_PICTURE = 2;
+    int REQUEST_CROP_STORE_NAME = 3;
+    int REQUEST_CROP_ITEMS = 4;
+    Uri imageUri, receiptStoreName, receiptItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +27,7 @@ public class TakePhotoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
 
-        OCRActivity     = (Button)      findViewById(R.id.OCRActivity);
         takePicture     = (Button)      findViewById(R.id.picture);
-        cropPicture     = (ImageButton) findViewById(R.id.crop_image);
-        imagePreview    = (ImageView)   findViewById(R.id.previewImage);
-        imagePreview.setVisibility(View.INVISIBLE);
 
         //Begin the activity in take-picture mode
         //if (settings.startinpicturemodesetting) {}
@@ -57,66 +48,67 @@ public class TakePhotoActivity extends Activity {
                 }
             }
         });
-        //Button for cropping
-        cropPicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (imagePreview.getVisibility() == View.VISIBLE) {
-                    Uri croppedImage = imageUri;
-                    CropImageIntentBuilder cropImage = new CropImageIntentBuilder(200, 200, croppedImage);
-                    cropImage.setOutlineColor(0xFF03A9F4);
-                    cropImage.setSourceImage(croppedImage);
-                    startActivityForResult(cropImage.getIntent(getApplicationContext()), CAMERA_CROP_REQUEST);
-                }
-
-                else {
-                    Toast toast = Toast.makeText(getApplicationContext(), "No Image!", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-            }
-        });
-
-        //Button for launching OCR
-        OCRActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (imagePreview.getVisibility() == View.VISIBLE) {
-
-                    Intent OCRIntent = new Intent(getApplicationContext(), OCRActivity.class);
-                    OCRIntent.putExtra("ImageURI", imageUri);
-                    startActivity(OCRIntent);
-                }
-
-                else {
-
-                    Toast toast = Toast.makeText(getApplicationContext(), "No Image!", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-            }
-        });
-
     }
 
 
+    protected void startCrop(Uri imageToCrop){
+        //Initialize CropImage intent with a 200x200 rectangle, outputting to imageToCrop URI
+        CropImageIntentBuilder cropImage = new CropImageIntentBuilder(200, 200, imageToCrop);
+        //Todo: set in colors xml
+        cropImage.setOutlineColor(0xFF03A9F4);
+        //Don't need face detection for cropping - it was set on by default
+        cropImage.setDoFaceDetection(false);
+        //set the source image as imageToCrop
+        cropImage.setSourceImage(imageToCrop);
+        startActivityForResult(cropImage.getIntent(this), REQUEST_CROP_PICTURE);
+    }
+
+    //TODO: Get both rectangles on one screen
+    protected void cropReceiptStoreName(Uri imageToPrecision){
+        CropImageIntentBuilder cropImage = new CropImageIntentBuilder(200, 100, receiptStoreName);
+        cropImage.setOutlineColor(0xFF03A9F4);
+        cropImage.setSourceImage(imageToPrecision);
+        startActivityForResult(cropImage.getIntent(this), REQUEST_CROP_PICTURE);
+    }
+
+    protected void cropReceiptItems(Uri imageToPrecision){
+        CropImageIntentBuilder cropImage = new CropImageIntentBuilder(200, 100, receiptItems);
+        cropImage.setOutlineColor(0xFF03A9F4);
+        cropImage.setSourceImage(imageToPrecision);
+        startActivityForResult(cropImage.getIntent(this), REQUEST_CROP_PICTURE);
+    }
+
+
+    protected void startPrecisionRectangles(Uri imageToPrecision){
+        cropReceiptStoreName(imageToPrecision);
+        cropReceiptItems(imageToPrecision);
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         if (resultCode == RESULT_OK) {
-
             if (requestCode == CAMERA_PIC_REQUEST) {
-                //Todo: Encapsulate
                 imageUri = data.getData();
                 imagePreview.setImageURI(imageUri);
-                if (imagePreview.getVisibility() == View.INVISIBLE)
-                    imagePreview.setVisibility(View.VISIBLE);
-
+                startCrop(imageUri);
             }
 
-            else if (requestCode == CAMERA_CROP_REQUEST){
+            else if (requestCode == REQUEST_CROP_PICTURE){
+                imageUri = data.getData();
                 imagePreview.setImageURI(imageUri);
-                //do a crop
+                startPrecisionRectangles(imageUri);
             }
+
+            else if (requestCode == REQUEST_CROP_STORE_NAME){
+                imageUri = data.getData();
+                imagePreview.setImageURI(imageUri);
+
+            }
+
+            else if (requestCode == REQUEST_CROP_ITEMS){
+                imageUri = data.getData();
+                imagePreview.setImageURI(imageUri);
+            }
+
         }
     }
 
