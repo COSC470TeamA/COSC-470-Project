0e2c5effc9799ab36249a69248062730d12b78bc
Breanne Wiebe <brewiebe@gmail.com>
2016-11-24 14:43:55 -0800

    Add Item to Receipt in Receipt Factory

--- RTracker/app/src/main/java/com/christopheramazurgmail/rtracker/ReceiptFactory.java
+++ RTracker/app/src/main/java/com/christopheramazurgmail/rtracker/ReceiptFactory.java
@@ -1,17 +1,29 @@
 package com.christopheramazurgmail.rtracker;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.Spinner;
+import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.christopheramazurgmail.rtracker.adapters.LongClickEditDialogListener;
 import com.christopheramazurgmail.rtracker.adapters.ReceiptFactoryListAdapter;
 
 import java.util.ArrayList;
@@ -21,6 +33,8 @@ import java.util.LinkedList;
 public class ReceiptFactory extends AppCompatActivity {
     private CategorizationEngine categorizationEngine;
     private Receipt receipt;
+    private ArrayList<Item> itemArrayList;
+    private ReceiptFactoryListAdapter adapter;
 
     public void start(Context context, Receipt receipt, CategorizationEngine categorizationEngine) {
         Intent intent = new Intent(context, ReceiptFactory.class);
@@ -42,6 +56,7 @@ public class ReceiptFactory extends AppCompatActivity {
         //populate store name
         TextView storeName = (TextView) findViewById(R.id.store_name);
         storeName.setText((String) getIntent().getSerializableExtra("StoreName"));
+        storeName.setOnLongClickListener(new LongClickEditDialogListener());
 
         //populate & initialize tag all spinner
         Spinner tagAllSpinner = (Spinner) findViewById(R.id.tag_all_spinner);
@@ -66,11 +81,22 @@ public class ReceiptFactory extends AppCompatActivity {
         });
 
         //populate items
-        initializeItems((ArrayList<Item>)getIntent().getSerializableExtra("Items"));
+        this.itemArrayList = (ArrayList<Item>)getIntent().getSerializableExtra("Items");
+        initializeItems(this.itemArrayList);
 
         //populate total
         TextView totalPriceView = (TextView) findViewById(R.id.total_price);
         totalPriceView.setText(String.format("$%.2f", getTotal()));
+
+        //update total on callback
+        Message message = new Message();
+
+        //allows text to be editable
+        totalPriceView.setOnLongClickListener(new LongClickEditDialogListener());
+
+        //set + action
+        Button addItemButton = (Button) findViewById(R.id.add_item_button);
+        addItemButton.setOnClickListener(new OnClickAddItemDialog());
 
         //set save actions
         Button saveButton = (Button) findViewById(R.id.save_button);
@@ -104,9 +130,16 @@ public class ReceiptFactory extends AppCompatActivity {
 
     private void initializeItems(ArrayList<Item> itemsList) {
         ListView listView = (ListView) findViewById(R.id.item_list);
-        ReceiptFactoryListAdapter adapter = new ReceiptFactoryListAdapter(this,
+        this.adapter = new ReceiptFactoryListAdapter(this,
                 R.layout.content_item_receipt_factory, itemsList, categorizationEngine.getCategories());
         listView.setAdapter(adapter);
+        adapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                updateTotal();
+            }
+        });
     }
 
     private ArrayList<Item> getModifiedReceiptItems() {
@@ -135,7 +168,10 @@ public class ReceiptFactory extends AppCompatActivity {
     }
 
     private void applyChanges() {
+        TextView storeName = (TextView) findViewById(R.id.store_name);
+        this.receipt.setStore(storeName.getText().toString());
         this.receipt.items.setItems(getModifiedReceiptItems());
+        //TODO write to DB
     }
 
     public Receipt getReceipt() {
@@ -156,4 +192,52 @@ public class ReceiptFactory extends AppCompatActivity {
 
         return arraySpinner;
     }
+
+    private void updateTotal() {
+        TextView totalPriceView = (TextView) findViewById(R.id.total_price);
+        totalPriceView.setText(String.format("$%.2f", getTotal()));
+    }
+
+    private class OnClickAddItemDialog implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setTitle("Add Item");
+            // Get the layout inflater
+            LayoutInflater inflater = (LayoutInflater) v.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            View view = inflater.inflate(R.layout.add_item_dialog, null);
+            builder.setView(view);
+            //Display text of calling text view in edit field
+            final EditText itemDescEditText = (EditText) view.findViewById(R.id.item_name_edit_text);
+            final EditText itemPriceEditText = (EditText) view.findViewById(R.id.item_price_edit_text);
+
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    try {
+                        String itemPrice = itemPriceEditText.getText().toString().replace("$", "");
+                        double priceDouble = Double.parseDouble(itemPrice);
+                        String itemDesc = itemDescEditText.getText().toString();
+                        itemArrayList.add(new Item(itemDesc, priceDouble));
+                        adapter.notifyDataSetChanged();
+                        updateTotal();
+                        dialog.dismiss();
+                    } catch (NumberFormatException nfe) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Not a Valid Price", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            //Get the AlertDialog from create()
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
 }
--- RTracker/app/src/main/java/com/christopheramazurgmail/rtracker/adapters/ReceiptFactoryListAdapter.java
+++ RTracker/app/src/main/java/com/christopheramazurgmail/rtracker/adapters/ReceiptFactoryListAdapter.java
@@ -1,13 +1,17 @@
 package com.christopheramazurgmail.rtracker.adapters;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.christopheramazurgmail.rtracker.Category;
 import com.christopheramazurgmail.rtracker.Item;
@@ -56,9 +60,83 @@ public class ReceiptFactoryListAdapter extends ArrayAdapter<Item> {
             holder.itemDesc.setText(getItem(i).getDesc());
         }
 
+        holder.itemDesc.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("Edit Field");
+                // Get the layout inflater
+                LayoutInflater inflater = (LayoutInflater) v.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                // Inflate and set the layout for the dialog
+                // Pass null as the parent view because its going in the dialog layout
+                View view = inflater.inflate(R.layout.edit_text_dialog, null);
+                builder.setView(view);
+                //Display text of calling text view in edit field
+                final TextView textToEdit = (TextView) v;
+                final EditText editText = (EditText) view.findViewById(R.id.text_to_edit);
+                editText.setText(textToEdit.getText());
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        getItem(item).setDesc(editText.getText().toString());
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+                //Get the AlertDialog from create()
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
+        });
+
         if(getItem(i).getPrice() != null) {
             holder.itemPrice.setText("$" + getItem(i).getPrice());
         }
+
+        holder.itemPrice.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("Edit Field");
+                // Get the layout inflater
+                LayoutInflater inflater = (LayoutInflater) v.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                // Inflate and set the layout for the dialog
+                // Pass null as the parent view because its going in the dialog layout
+                View view = inflater.inflate(R.layout.edit_text_dialog, null);
+                builder.setView(view);
+                //Display text of calling text view in edit field
+                final TextView textToEdit = (TextView) v;
+                final EditText editText = (EditText) view.findViewById(R.id.text_to_edit);
+                editText.setText(textToEdit.getText());
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        try {
+                            String editedPrice = editText.getText().toString().replace("$", "");
+                            double price = Double.parseDouble(editedPrice);
+                            getItem(item).setPrice(price);
+                            notifyDataSetChanged();
+                            dialog.dismiss();
+                        } catch (NumberFormatException nfe) {
+                            Toast toast = Toast.makeText(getContext(), "Not a Valid Price", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+                //Get the AlertDialog from create()
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
+        });
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, arraySpinner);
         holder.itemSpinner.setAdapter(adapter);
@@ -105,5 +183,10 @@ public class ReceiptFactoryListAdapter extends ArrayAdapter<Item> {
         TextView itemDesc;
         TextView itemPrice;
         Spinner itemSpinner;
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
     }
 }
